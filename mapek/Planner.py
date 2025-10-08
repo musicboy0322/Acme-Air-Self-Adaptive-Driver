c7933de6efcfebe8fdae09a819f29b0053c1a429
musicboy0322 <plusultra0322@gmail.com>
2025-10-08 11:49:54 -0400

    delete duplcated code

--- mapek/Planner.py
+++ mapek/Planner.py
@@ -78,45 +78,6 @@ class Planner:
                 else:
                     new_config["replica"] = max(new_config["replica"] - 1, self.min_replica)
                     adaptations.append("decrease_replica")
-            
-            for action in analysis_result["adaptation"]:
-                if action == "increase cpu" and new_config["cpu"] < self.max_cpu:
-                    new_config["cpu"] = min(new_config["cpu"] + 200, self.max_cpu)
-                    adaptations.append(action)
-                elif action == "increase memory" and new_config["memory"] < self.max_memory:
-                    new_config["memory"] = min(new_config["memory"] + 256, self.max_memory)
-                    adaptations.append(action)
-                elif action == "increase replica":
-                    if new_config["replica"] >= self.max_replica:
-                        print("Unable to increase replica: Reached maximum")
-                    else:
-                        new_config["replica"] = min(new_config["replica"] + 1, self.max_replica)
-                        adaptations.append(action)
-                elif action == "decrease cpu":
-                    if new_config["cpu"] <= self.min_cpu:
-                        print("Unable to decrease cpu: Reached minimum")
-                    else:
-                        new_config["cpu"] = max(new_config["cpu"] - 200, self.min_cpu)
-                        adaptations.append(action)  
-                elif action == "decrease memory":
-                    if new_config["memory"] <= self.min_memory:
-                        print("Unable to decrease memory: Reached minimum")
-                    else:
-                        new_config["memory"] = max(new_config["memory"] - 256, self.min_memory)
-                        adaptations.append(action)
-                elif action == "decrease replica":
-                    if new_config["replica"] <= 1:
-                        print("Unable to decrease replica: Reached minimum")
-                        print("Trying to decrease cpu and memory if possible")
-                        if new_config["cpu"] > self.min_cpu:
-                            new_config["cpu"] = max(new_config["cpu"] - 200, self.min_cpu)
-                            adaptations.append("decrease cpu")
-                        if new_config["memory"] > self.min_memory:
-                            new_config["memory"] = max(new_config["memory"] - 256, self.min_memory)
-                            adaptations.append("decrease memory")
-                    else:
-                        new_config["replica"] = max(new_config["replica"] - 1, 1)
-                        adaptations.append(action)
         
         return new_config if adaptations else None
     
